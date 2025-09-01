9f73fd62884648d60a174bd995b96ad622472575
Huzaifa Khan <hmzkhanhmzk@gmail.com>
2025-09-01 10:37:04 +0530

    modified some code

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5500;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send({'Hello World!' });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
